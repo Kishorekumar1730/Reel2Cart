b8f6644a1ac8adecb5a8f9496a68b0d34f7ac621
Kishorekumar1730 <kishorekumarv25@gmail.com>
2026-01-02 20:22:41 +0530

    Save current status

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://reel-2-cart-oovbv3obh-kishorekumars-projects-7803047f.vercel.app'; // Production URL
+export const API_BASE_URL = 'https://reel-2-cart.vercel.app'; // Production URL
 // export const API_BASE_URL = 'http://10.250.155.200:8000'; // Physical Device (LAN IP)
 // export const API_BASE_URL = 'http://10.0.2.2:8000'; // Android Emulator
 // export const API_BASE_URL = 'http://localhost:8000'; // iOS Simulator
